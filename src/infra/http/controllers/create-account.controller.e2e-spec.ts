75b8df109919d7248ab61d5f58f6a3b7bc191d81
Gustavo-Kissel <kisselgustavo@gmail.com>
2024-02-06 14:36:16 -0300

    gkissel : Mapper

--- src/infra/http/controllers/create-account.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-account.controller.e2e-spec.ts
@@ -1,8 +1,8 @@
-import { AppModule } from '@/app.module'
-import { PrismaService } from '@/prisma/prisma.service'
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { AppModule } from '@/infra/app.module'
 
 describe('Create Account (E2E)', () => {
   let app: INestApplication
--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { CurrentUser } from '@/auth/current-user-decorator'
-import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
-import { UserPayload } from '@/auth/jwt.strategy'
-import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
-import { PrismaService } from '@/prisma/prisma.service'
+
 import { z } from 'zod'
+import { CurrentUser } from '../../auth/current-user-decorator'
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard'
+import { UserPayload } from '../../auth/jwt.strategy'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
